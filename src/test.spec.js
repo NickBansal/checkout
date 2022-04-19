3f796ec50e41574a67cb2cccaba4632a9f4fc2d4
Nick Bansal <extreme_wired@msn.com>
2022-04-19 16:32:07 +0100

    added in the first failing test

--- src/test.spec.js
+++ src/test.spec.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('<Checkout />', () => {
   it('should show a header', () => {
     render(<App />);
-
     expect(screen.getByText('Checkout')).toBeInTheDocument();
+  });
+
+  it('should update the total of face masks when a face mask is added', () => {
+    render(<App />);
+    userEvent.click(screen.getAllByRole('button')[0]);
+    expect(screen.getByText('2.50')).toBeInTheDocument();
   });
 });
