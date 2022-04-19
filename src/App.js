acf920fa7b36c5ebd0d77637af3dde66b974cb2f
Nick Bansal <extreme_wired@msn.com>
2022-04-19 15:09:14 +0100

    initialised tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>CHECKOUT</h1>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="w-full bg-teal-500 p-4">Header</header>
+  );
+}
