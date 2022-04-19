d609ca8f2921609f328b4cf1dd25dffaa06874c3
Nick Bansal <extreme_wired@msn.com>
2022-04-19 14:20:18 +0100

    initiated eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-<h1>CHECKOUT</h1>
+        <h1>CHECKOUT</h1>
       </header>
     </div>
   );
