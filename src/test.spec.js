8143f1aced9be3f7d44b4c358566ad4d9b690c48
Nick Bansal <extreme_wired@msn.com>
2022-04-19 20:58:26 +0100

    updated the tests

--- src/test.spec.js
+++ src/test.spec.js
@@ -12,29 +12,29 @@ describe('<Checkout />', () => {
   it('should update the total of face masks when a face mask is added', () => {
     render(<App />);
     userEvent.click(screen.getAllByRole('button')[0]);
-    expect(screen.getByText('Face Masks (1)')).toBeInTheDocument();
+    expect(screen.getByText('(1)')).toBeInTheDocument();
   });
 
   it('should update the total of face masks when a face mask is subtracted', () => {
     render(<App />);
     userEvent.dblClick(screen.getAllByRole('button')[0]);
-    expect(screen.getByText('Face Masks (2)')).toBeInTheDocument();
+    expect(screen.getByText('(2)')).toBeInTheDocument();
     userEvent.click(screen.getAllByRole('button')[1]);
-    expect(screen.getByText('Face Masks (1)')).toBeInTheDocument();
+    expect(screen.getByText('(1)')).toBeInTheDocument();
   });
 
   it('should update the total of toilet paper when a toilet paper is added', () => {
     render(<App />);
     userEvent.click(screen.getAllByRole('button')[2]);
-    expect(screen.getByText('Toilet Paper (1)')).toBeInTheDocument();
+    expect(screen.getByText('(1)')).toBeInTheDocument();
   });
 
   it('should update the total of face masks when a face mask is subtracted', () => {
     render(<App />);
     userEvent.dblClick(screen.getAllByRole('button')[2]);
-    expect(screen.getByText('Toilet Paper (2)')).toBeInTheDocument();
+    expect(screen.getByText('(2)')).toBeInTheDocument();
     userEvent.click(screen.getAllByRole('button')[3]);
-    expect(screen.getByText('Toilet Paper (1)')).toBeInTheDocument();
+    expect(screen.getByText('(1)')).toBeInTheDocument();
   });
 
   it('should not allow the user to go below zero and it should disable to button', () => {
@@ -49,32 +49,34 @@ describe('<Checkout />', () => {
 
   it('should increase the total price when a face mask is added', () => {
     render(<App />);
-    userEvent.click(screen.getAllByRole('button')[0]);
-    expect(screen.getByText('£2.50')).toBeInTheDocument();
     userEvent.dblClick(screen.getAllByRole('button')[0]);
+    expect(screen.getByText('£5.00')).toBeInTheDocument();
+    userEvent.click(screen.getAllByRole('button')[0]);
     expect(screen.getByText('£7.50')).toBeInTheDocument();
   });
 
   it('should increase the total price when a toilet paper is added', () => {
     render(<App />);
-    userEvent.click(screen.getAllByRole('button')[2]);
-    expect(screen.getByText('£0.65')).toBeInTheDocument();
     userEvent.dblClick(screen.getAllByRole('button')[2]);
+    expect(screen.getByText('£1.30')).toBeInTheDocument();
+    userEvent.click(screen.getAllByRole('button')[2]);
     expect(screen.getByText('£1.95')).toBeInTheDocument();
   });
 
   it('should decrease the total price when a face mask is subtracted', () => {
     render(<App />);
     userEvent.dblClick(screen.getAllByRole('button')[0]);
+    userEvent.click(screen.getAllByRole('button')[0]);
     userEvent.click(screen.getAllByRole('button')[1]);
-    expect(screen.getByText('£2.50')).toBeInTheDocument();
+    expect(screen.getByText('£5.00')).toBeInTheDocument();
   });
 
   it('should decrease the total price when a toilet paper is subtracted', () => {
     render(<App />);
     userEvent.dblClick(screen.getAllByRole('button')[2]);
+    userEvent.click(screen.getAllByRole('button')[2]);
     userEvent.click(screen.getAllByRole('button')[3]);
-    expect(screen.getByText('£0.65')).toBeInTheDocument();
+    expect(screen.getByText('£1.30')).toBeInTheDocument();
   });
 
   it('should apply a discount on face masks, two for £4', () => {
