ea8776081ddb15b72c8b594f8daa95c6ec5b9fe5
Nick Bansal <extreme_wired@msn.com>
2022-04-20 07:28:11 +0100

    updated the variable names

--- src/test.spec.js
+++ src/test.spec.js
@@ -20,6 +20,7 @@ describe('<Checkout />', () => {
   it('should update the total of face masks when a face mask is subtracted', () => {
     render(<App />);
     const { getByText, getByTestId } = screen;
+
     userEvent.dblClick(getByTestId('addBtn0'));
     expect(getByText('Face Masks (2)')).toBeInTheDocument();
     userEvent.click(getByTestId('minusBtn0'));
@@ -29,6 +30,7 @@ describe('<Checkout />', () => {
   it('should update the total of toilet paper when a toilet paper is added', () => {
     render(<App />);
     const { getByText, getByTestId } = screen;
+
     userEvent.click(getByTestId('addBtn1'));
     expect(getByText('Toilet Paper (1)')).toBeInTheDocument();
   });
@@ -36,6 +38,7 @@ describe('<Checkout />', () => {
   it('should update the total of toilet paper when a toilet paper is subtracted', () => {
     render(<App />);
     const { getByText, getByTestId } = screen;
+
     userEvent.dblClick(getByTestId('addBtn1'));
     expect(getByText('Toilet Paper (2)')).toBeInTheDocument();
     userEvent.click(getByTestId('minusBtn1'));
@@ -45,6 +48,7 @@ describe('<Checkout />', () => {
   it('should not allow the user to go below zero and it should disable to minus buttons', () => {
     render(<App />);
     const { getByTestId } = screen;
+
     expect(getByTestId('minusBtn0')).toBeDisabled();
     expect(getByTestId('minusBtn1')).toBeDisabled();
     userEvent.click(getByTestId('addBtn0'));
@@ -56,6 +60,7 @@ describe('<Checkout />', () => {
   it('should increase the total price when a face mask is added', () => {
     render(<App />);
     const { getByText, getByTestId } = screen;
+
     userEvent.dblClick(getByTestId('addBtn0'));
     expect(getByText('£5.00')).toBeInTheDocument();
     userEvent.click(getByTestId('addBtn0'));
@@ -65,85 +70,85 @@ describe('<Checkout />', () => {
   it('should increase the total price when a toilet paper is added', () => {
     render(<App />);
     const { getByTestId, getAllByRole } = screen;
-    const listItem = getAllByRole('listitem')[3];
+    const totalAmount = getAllByRole('listitem')[3];
 
     userEvent.dblClick(getByTestId('addBtn1'));
-    expect(within(listItem).getByText('£1.30')).toBeInTheDocument();
+    expect(within(totalAmount).getByText('£1.30')).toBeInTheDocument();
     userEvent.click(getByTestId('addBtn1'));
-    expect(within(listItem).getByText('£1.95')).toBeInTheDocument();
+    expect(within(totalAmount).getByText('£1.95')).toBeInTheDocument();
   });
 
   it('should decrease the total price when a face mask is subtracted', () => {
     render(<App />);
     const { getByTestId, getAllByRole } = screen;
-    const listItem = getAllByRole('listitem')[3];
+    const totalAmount = getAllByRole('listitem')[3];
 
     userEvent.dblClick(getByTestId('addBtn0'));
     userEvent.click(getByTestId('addBtn0'));
     userEvent.click(getByTestId('minusBtn0'));
-    expect(within(listItem).getByText('£5.00')).toBeInTheDocument();
+    expect(within(totalAmount).getByText('£5.00')).toBeInTheDocument();
   });
 
   it('should decrease the total price when a toilet paper is subtracted', () => {
     render(<App />);
     const { getAllByRole, getByTestId } = screen;
-    const listItem = getAllByRole('listitem')[3];
+    const totalAmount = getAllByRole('listitem')[3];
 
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.click(getByTestId('addBtn1'));
     userEvent.click(getByTestId('minusBtn1'));
-    expect(within(listItem).getByText('£1.30')).toBeInTheDocument();
+    expect(within(totalAmount).getByText('£1.30')).toBeInTheDocument();
   });
 
   it('should apply a discount on face masks, two for £4', () => {
     render(<App />);
     const { getAllByRole, getByTestId } = screen;
-    const listItem = getAllByRole('listitem')[1];
+    const faceMasks = getAllByRole('listitem')[1];
 
-    expect(within(listItem).getByText('£0.00')).toBeInTheDocument();
+    expect(within(faceMasks).getByText('£0.00')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £1.00')).toBeInTheDocument();
+    expect(within(faceMasks).getByText('- £1.00')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £2.00')).toBeInTheDocument();
+    expect(within(faceMasks).getByText('- £2.00')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £3.00')).toBeInTheDocument();
+    expect(within(faceMasks).getByText('- £3.00')).toBeInTheDocument();
   });
 
   it('should take away the face masks discount if items are deleted', () => {
     render(<App />);
     const { getAllByRole, getByTestId } = screen;
-    const listItem = getAllByRole('listitem')[1];
+    const faceMasks = getAllByRole('listitem')[1];
 
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £1.00')).toBeInTheDocument();
+    expect(within(faceMasks).getByText('- £1.00')).toBeInTheDocument();
     userEvent.click(getByTestId('minusBtn0'));
-    expect(within(listItem).queryByText('- £1.00')).not.toBeInTheDocument();
+    expect(within(faceMasks).queryByText('- £1.00')).not.toBeInTheDocument();
   });
 
   it('should apply a discount on toilet paper, 6 for 5', () => {
     render(<App />);
     const { getByTestId, getAllByRole } = screen;
-    const listItem = getAllByRole('listitem')[2];
+    const toiletPapers = getAllByRole('listitem')[2];
 
-    expect(within(listItem).getByText('£0.00')).toBeInTheDocument();
+    expect(within(toiletPapers).getByText('£0.00')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
-    expect(within(listItem).getByText('- £0.65')).toBeInTheDocument();
+    expect(within(toiletPapers).getByText('- £0.65')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
-    expect(within(listItem).getByText('- £1.30')).toBeInTheDocument();
+    expect(within(toiletPapers).getByText('- £1.30')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
-    expect(within(listItem).getByText('- £1.95')).toBeInTheDocument();
+    expect(within(toiletPapers).getByText('- £1.95')).toBeInTheDocument();
   });
 
   it('should take away the toilet paper discount if items are deleted', () => {
     render(<App />);
     const { getAllByRole, getByTestId, queryByText } = screen;
-    const listItem = getAllByRole('listitem')[2];
+    const toiletPapers = getAllByRole('listitem')[2];
 
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
@@ -151,44 +156,44 @@ describe('<Checkout />', () => {
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
-    expect(within(listItem).getByText('- £1.30')).toBeInTheDocument();
+    expect(within(toiletPapers).getByText('- £1.30')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('minusBtn1'));
     expect(queryByText('- £1.30')).not.toBeInTheDocument();
-    expect(within(listItem).getByText('- £0.65')).toBeInTheDocument();
+    expect(within(toiletPapers).getByText('- £0.65')).toBeInTheDocument();
   });
 
   it('should tally up the total discounts', () => {
     render(<App />);
     const { getAllByRole, getByTestId } = screen;
-    const listItem = getAllByRole('listitem')[4];
+    const totalDiscounts = getAllByRole('listitem')[4];
 
-    expect(within(listItem).getByText('£0.00')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('£0.00')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £1.65')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('- £1.65')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £2.65')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('- £2.65')).toBeInTheDocument();
   });
 
   it('should tally down the total discounts', () => {
     render(<App />);
     const { getAllByRole, getByTestId } = screen;
-    const listItem = getAllByRole('listitem')[4];
+    const totalDiscounts = getAllByRole('listitem')[4];
 
-    expect(within(listItem).getByText('£0.00')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('£0.00')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn1'));
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £1.65')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('- £1.65')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('addBtn0'));
-    expect(within(listItem).getByText('- £2.65')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('- £2.65')).toBeInTheDocument();
     userEvent.dblClick(getByTestId('minusBtn1'));
     userEvent.dblClick(getByTestId('minusBtn1'));
     userEvent.dblClick(getByTestId('minusBtn1'));
-    expect(within(listItem).getByText('- £2.00')).toBeInTheDocument();
+    expect(within(totalDiscounts).getByText('- £2.00')).toBeInTheDocument();
   });
 
   it('should give the user a total amount with discounts taken away', () => {
