e8e2a52851eb4ab2a92a57df6e2d0ffed7c1b70c
Nick Bansal <extreme_wired@msn.com>
2022-04-20 10:01:47 +0100

    condensed the functions

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -8,6 +8,9 @@ const buttonWrapper = 'border-dotted border-black border-l-2 pl-4 w-1/3 md:w-1/4
 const buttonItem = 'mx-1 my-1 bg-orange-200 hover:bg-orange-600 transition-colors text-base md:text-xl px-0 md:px-2 rounded-md border border-solid w-[40px]';
 const disabledBtn = 'cursor-not-allowed bg-gray-200 mx-1 text-base md:text-xl px-0 md:px-2 rounded-md border border-solid w-[40px] text-white';
 
+const addItems = (a, b) => a + b;
+const subtractItems = (a, b) => a - b;
+
 export default function Checkout() {
   const shoppingItems = Object.keys(itemPrices);
 
@@ -26,59 +29,36 @@ export default function Checkout() {
     }, {}));
   }, []);
 
-  const addItemDiscounts = (item, previous, quantity) => {
-    if (item === 'Face Masks' && quantity % 2 === 0) {
-      setTotalDiscount(totalDiscount + 1);
-      return previous + 1;
+  const itemDiscounts = (item, previous, quantity, add) => {
+    const sum = add ? addItems : subtractItems;
+    const totalQuantity = add ? quantity : quantity + 1;
+
+    if (item === 'Face Masks' && totalQuantity % 2 === 0) {
+      setTotalDiscount(sum(totalDiscount, 1));
+      return sum(previous, 1);
     }
-    if (item === 'Toilet Paper' && quantity % 6 === 0) {
-      setTotalDiscount(totalDiscount + 0.65);
-      return previous + 0.65;
+    if (item === 'Toilet Paper' && totalQuantity % 6 === 0) {
+      setTotalDiscount(sum(totalDiscount, 0.65));
+      return sum(previous, 0.65);
     }
     return previous;
   };
 
-  const subtractItemDiscounts = (item, previous, quantity) => {
-    // const lowerAmount = quantity < items[item].quantity;
-    if (item === 'Face Masks' && (quantity - 1) % 2 === 0) {
-      setTotalDiscount(totalDiscount - 1);
-      return previous - 1;
-    }
-    if (item === 'Toilet Paper' && (quantity + 1) % 6 === 0) {
-      setTotalDiscount(totalDiscount - 0.65);
-      return previous - 0.65;
-    }
-    return previous;
-  };
+  const updateBasket = (item, values, add = true) => {
+    const sum = add ? addItems : subtractItems;
 
-  const addToBasket = (item, values) => {
     setItems((prevState) => {
-      const quantity = prevState[item].quantity + 1;
+      const quantity = sum(prevState[item].quantity, 1);
       return ({
         ...prevState,
         [item]: {
           ...values,
           quantity,
-          discount: addItemDiscounts(item, prevState[item].discount, quantity),
+          discount: itemDiscounts(item, prevState[item].discount, quantity, add),
         },
       });
     });
-    setTotal((Number(total) + Number(itemPrices[item])).toFixed(2));
-  };
-
-  const subtractFromBasket = (item, values) => {
-    setItems((prevState) => {
-      const quantity = prevState[item].quantity - 1;
-      return ({
-        ...prevState,
-        [item]: {
-          ...values,
-          quantity,
-          discount: subtractItemDiscounts(item, prevState[item].discount, quantity),
-        },
-      });
-    });
-    setTotal((Number(total) - Number(itemPrices[item])).toFixed(2));
+    setTotal(sum(Number(total), Number(itemPrices[item])).toFixed(2));
   };
 
   if (items === undefined) {
@@ -124,7 +104,7 @@ export default function Checkout() {
                     type="button"
                     data-testid={`addBtn${index}`}
                     className={buttonItem}
-                    onClick={() => addToBasket(item, items[item])}
+                    onClick={() => updateBasket(item, items[item])}
                   >
                     +
 
@@ -133,7 +113,7 @@ export default function Checkout() {
                     type="button"
                     data-testid={`minusBtn${index}`}
                     className={disabled ? disabledBtn : buttonItem}
-                    onClick={() => subtractFromBasket(item, items[item])}
+                    onClick={() => updateBasket(item, items[item], false)}
                     disabled={disabled}
                   >
                     -
